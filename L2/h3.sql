4b02be0a1cb681bff15284d62fd3c770c479c60f
Syzen <58214168+Syzen4869@users.noreply.github.com>
2022-06-03 19:20:06 +0700

    Add files via upload

---
+++ L2/h3.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Store (
+  ID VARCHAR (10),
+  Name VARCHAR (50),
+  Adress VARCHAR (100),
+);
+
+INSERT INTO Store VALUES
+('1', 'AMC1', '100 Naragoya Rd'),
+('2', 'AMC2', '200 Naragoya Rd'),
+('3', 'AMC3', '300 Naragoya Rd'),
+('4', 'AMC4', '400 Naragoya Rd'),
+('5', 'AMC5', '500 Naragoya Rd'),
+('6', 'AMC6', '600 Naragoya Rd'),
+('7', 'AMC7', '700 Naragoya Rd'),
+('8', 'AMC8', '800 Naragoya Rd'),
+('9', 'AMC9', '900 Naragoya Rd'),
+('10', 'AMC10', '1000 Naragoya Rd');
